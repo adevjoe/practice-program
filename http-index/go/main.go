91ce8edf123ca93f939113b1ac5e8c495b2b133d
adevjoe <adevjoe@gmail.com>
2020-03-19 10:38:31 +0800

    add port flag

--- http-index/go/main.go
+++ http-index/go/main.go
@@ -9,8 +9,10 @@ var Root *string
 
 func main() {
 	Root = flag.String("root", "./www", "handle file path")
+	port := flag.String("port", "8080", "http port")
 	flag.Parse()
 	log.Println("http server starting...")
 	log.Printf("args root: %s", *Root)
-	log.Fatal(HttpServerAndListen(":8080", ))
+	log.Printf("http listen on http://127.0.0.1:%s", *port)
+	log.Fatal(HttpServerAndListen(":" + *port))
 }
